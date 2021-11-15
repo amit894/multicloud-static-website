21ca7fa841b4af440f5faaaea07af2a6980f27ee
Amit Raj <amit.894@gmail.com>
2021-11-15 09:18:57 +0530

    Base Storage Account

--- modules/azure/main.tf
+++ modules/azure/main.tf
@@ -6,3 +6,24 @@ terraform {
     }
   }
 }
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "amit-example-resources"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "amit894account"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "dev"
+  }
+}
