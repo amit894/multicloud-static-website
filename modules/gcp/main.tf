78cc80ae20103b4d4915168c23b2215bca9910b9
Amit Raj <amit.894@gmail.com>
2021-11-15 17:00:57 +0530

    GCP Static Website Template

--- modules/gcp/main.tf
+++ modules/gcp/main.tf
@@ -1,4 +1,17 @@
 provider "google" {
-  region  = var.region
-  project = var.project_name
+  project     = "secure-proxy-308710"
+}
+
+# Bucket to store website
+resource "google_storage_bucket" "website" {
+  provider = google
+  name     = "amit894-website"
+  location = "US"
+}
+
+# Make new objects public
+resource "google_storage_default_object_access_control" "website_read" {
+  bucket = google_storage_bucket.website.name
+  role   = "READER"
+  entity = "allUsers"
 }
