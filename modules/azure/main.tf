8ba95c35eb3bbb8eeb946a2e73aa704072d20350
Amit Raj <amit.894@gmail.com>
2021-11-15 14:57:18 +0530

    Adding basic code for Static Webiste

--- modules/azure/main.tf
+++ modules/azure/main.tf
@@ -12,18 +12,34 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "amit-example-resources"
+  name     = "static-website-resources"
   location = "eastus"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "amit894account"
+  name                     = "staticwebsiteacnt894"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
+  account_kind             = "StorageV2"
   account_replication_type = "GRS"
+  enable_https_traffic_only = true
+
+  static_website {
+    index_document = "index.html"
+    }
+
 
   tags = {
     environment = "dev"
   }
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source_content         = "<h1>This is static content coming from the Terraform</h1>"
 }
