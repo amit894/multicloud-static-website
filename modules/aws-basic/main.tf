2de8f736bf1fcc3e54456b6defc662cf40834e47
Amit Raj <amit.894@gmail.com>
2021-11-16 10:24:31 +0530

    Code for AWS basic bucket creation

---
+++ modules/aws-basic/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.34.0"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "staticwebsiteacnt894"
+  acl = "public-read"
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
