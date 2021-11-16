2612ce8de1381ccabbcf02f37a3b946c6e691c53
Amit Raj <amit.894@gmail.com>
2021-11-16 10:05:56 +0530

    Basic Working Module for GCP Static Website

--- modules/gcp/main.tf
+++ modules/gcp/main.tf
@@ -9,6 +9,19 @@ resource "google_storage_bucket" "website" {
   location = "US"
 }
 
+resource "google_storage_bucket_object" "index" {
+  name    = "index.html"
+  content = "Hello, World!"
+  bucket  = google_storage_bucket.website.name
+}
+
+resource "google_storage_bucket_object" "not_found" {
+  name    = "404.html"
+  content = "Uh oh"
+  bucket  = google_storage_bucket.website.name
+}
+
+
 # Make new objects public
 resource "google_storage_default_object_access_control" "website_read" {
   bucket = google_storage_bucket.website.name
